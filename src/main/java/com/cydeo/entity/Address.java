e5fe08f84c474722500d0f499c7b8d2ad116fdd5
omrzcn <omrzcnn@gmail.com>
2024-07-23 00:33:25 +0300

    Adress entity created

--- src/main/java/com/cydeo/entity/Address.java
+++ src/main/java/com/cydeo/entity/Address.java
@@ -3,7 +3,17 @@ package com.cydeo.entity;
 import com.cydeo.entity.common.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "addresses")
 public class Address extends BaseEntity {
+
+    private String  address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
 }
