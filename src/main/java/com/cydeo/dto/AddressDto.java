bce8f235364e8d94a4c85a0af34a7861e8f1b026
omrzcn <omrzcnn@gmail.com>
2024-07-23 00:33:36 +0300

    Adress dto created

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -1,4 +1,12 @@
 package com.cydeo.dto;
 
 public class AddressDto {
+
+    private Long id;
+    private String  address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
 }
