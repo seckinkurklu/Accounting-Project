7cc39cc808664a5f92194bff9de313be6287a539
omrzcn <omrzcnn@gmail.com>
2024-07-23 01:01:02 +0300

    Adress  updated

--- src/main/java/com/cydeo/entity/Address.java
+++ src/main/java/com/cydeo/entity/Address.java
@@ -1,11 +1,20 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "addresses")
 public class Address extends BaseEntity {
 
