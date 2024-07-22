866c646c761166d468d723008b8338f2e3c3a090
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-23 01:21:57 +0300

    1-Invoice Product entity class is created

--- src/main/java/com/cydeo/entity/InvoiceProduct.java
+++ src/main/java/com/cydeo/entity/InvoiceProduct.java
@@ -1,9 +1,28 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class InvoiceProduct extends BaseEntity {
+    private int quantity;
+    private BigDecimal price;
+    private int tax;
+    private BigDecimal profitLoss;
+    private int remainingQuantity;
+    @ManyToOne
+    @JoinColumn(name = "invoice_id")
+    private Invoice invoice;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
