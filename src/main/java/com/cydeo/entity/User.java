f46e75c1fface98b6b36c0c58a1da3dbfd3a6926
Alan Uyghur <110825107+alanhoca@users.noreply.github.com>
2024-07-23 20:36:44 +0300

    JDGR10-12-FindByUsername, user mapper, user repository, user service and userserviceimpl, findbyusernanme created.

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,9 +1,28 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
 @Entity
 public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+
+    @ManyToOne
+    Role role=new Role();
+
+
 }
---
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.cydeo.mapper;
+
+
+import com.cydeo.dto.UserDto;
+import com.cydeo.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public User convertToEntity(UserDto dto){
+        return modelMapper.map(dto, User.class);
+    }
+
+    public UserDto convertToDto(User entity){
+        return modelMapper.map(entity, UserDto.class);
+    }
+}
