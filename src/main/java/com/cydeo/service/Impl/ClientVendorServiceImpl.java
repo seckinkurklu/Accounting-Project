5eb4e3008cee53c20650939418594969c67d22dc
omrzcn <omrzcnn@gmail.com>
2024-07-23 23:24:50 +0300

    ClientVendorService created, listALl method added

---
+++ src/main/java/com/cydeo/service/Impl/ClientVendorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.Impl;
+
+import com.cydeo.dto.ClientVendorDto;
+import com.cydeo.entity.ClientVendor;
+import com.cydeo.repository.ClientVendorRepository;
+import com.cydeo.service.ClientVendorService;
+import com.cydeo.util.MapperUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ClientVendorServiceImpl implements ClientVendorService {
+
+    private final ClientVendorRepository clientVendorRepository;
+    private final MapperUtil mapperUtil;
+
+    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, MapperUtil mapperUtil) {
+        this.clientVendorRepository = clientVendorRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public List<ClientVendorDto> listAllClientVendor() {
+        List<ClientVendor> clientVendors = clientVendorRepository.findAll();
+       return clientVendors.stream().map(clientVendor -> mapperUtil.convert(clientVendor,ClientVendorDto.class)).collect(Collectors.toList());
+
+    }
+}
