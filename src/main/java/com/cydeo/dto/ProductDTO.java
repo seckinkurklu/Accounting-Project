fb5505b484b229aea3a7e28c2df1ddcfff59350d
seckinkurklu <kurkluseckin@gmail.com>
2024-07-23 13:56:58 +0300

    Product DTO created

---
+++ src/main/java/com/cydeo/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.ProductUnit;
+
+public class ProductDTO {
+
+    private long id;
+    private String name;
+    private Integer quantityInStocks;
+    private Integer lowLimitAlert;
+    ProductUnit productUnit;
+    CategoryDto category;
+    boolean hasProduct;
+
+}
