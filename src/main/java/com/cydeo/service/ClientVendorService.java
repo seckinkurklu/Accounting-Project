be7c10c7ab809930002899dadb33498bcb1848a4
omrzcn <omrzcnn@gmail.com>
2024-07-23 23:24:32 +0300

    ClientVendorService created

---
+++ src/main/java/com/cydeo/service/ClientVendorService.java
@@ -0,0 +1,13 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ClientVendorDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface ClientVendorService {
+
+    List<ClientVendorDto> listAllClientVendor();
+
+
+}
