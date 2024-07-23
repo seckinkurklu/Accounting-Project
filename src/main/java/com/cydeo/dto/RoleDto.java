6123cdc64a66c5d611dea504ddc5ebea7fd9fb20
AbduShukur <abdushukur.sawut@gmail.com>
2024-07-23 18:00:36 +0300

    Create Role Entity & DTO Update

--- src/main/java/com/cydeo/dto/RoleDto.java
+++ src/main/java/com/cydeo/dto/RoleDto.java
@@ -1,4 +1,19 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleDto {
+
+    private Long id;
+
+    private String description;
+
 }
+
