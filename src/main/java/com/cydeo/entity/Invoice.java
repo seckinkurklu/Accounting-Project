4dfcc085c93319ba2d61e43ff3f93e263a1b8731
Alim <laysuinfo@gmail.com>
2024-07-23 11:33:47 +0300

    invoice Dto  class  is created

--- src/main/java/com/cydeo/entity/Invoice.java
+++ src/main/java/com/cydeo/entity/Invoice.java
@@ -1,9 +1,39 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "invoices")
 public class Invoice extends BaseEntity {
+
+    private String invoiceNo;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus invoiceStatus;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceType invoiceType;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "client_vendor_id")
+    private ClientVendor clientVendor;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
 }
