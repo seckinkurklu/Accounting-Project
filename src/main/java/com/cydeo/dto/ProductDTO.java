0bf2d8f1d6620124ddbddc5cb632d26eec8c67a8
seckinkurklu <kurkluseckin@gmail.com>
2024-07-23 14:02:38 +0300

    ProductUnit DTO created

--- src/main/java/com/cydeo/dto/ProductDTO.java
+++ src/main/java/com/cydeo/dto/ProductDTO.java
@@ -5,11 +5,18 @@ import com.cydeo.enums.ProductUnit;
 public class ProductDTO {
 
     private long id;
+
     private String name;
+
     private Integer quantityInStocks;
+
     private Integer lowLimitAlert;
+
     ProductUnit productUnit;
+
     CategoryDto category;
+
     boolean hasProduct;
+
 
 }
