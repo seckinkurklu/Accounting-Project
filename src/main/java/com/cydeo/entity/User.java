d24d2f2d9452a8fb46ce8f9bdb2ebfc28313e222
Alim <laysuinfo@gmail.com>
2024-07-24 09:39:08 +0300

    application properties updated

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -3,7 +3,9 @@ package com.cydeo.entity;
 import com.cydeo.entity.common.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "app_user")
 public class User extends BaseEntity {
 }
