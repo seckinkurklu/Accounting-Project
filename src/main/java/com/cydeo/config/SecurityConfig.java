1f05191b5b07bbb4f82e8a1027674fcfe7b2a4b1
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-24 16:18:09 +0300

    "Login" functionality unsuccessful logins, "Logout" functionality "Remember Me" functionality  are created

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -4,11 +4,12 @@ import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class SecurityConfig {// we create this Config class to create bigger Bean implementation, we can write as a Separate Config class
+public class SecurityConfig {
 
     private final SecurityService securityService;
     private final AuthSuccessHandler authSuccessHandler;
@@ -18,54 +19,37 @@ public class SecurityConfig {// we create this Config class to create bigger Bea
         this.authSuccessHandler = authSuccessHandler;
     }
 
-//    @Bean
-//    public UserDetailsService userDetailService (PasswordEncoder encoder) {
-//    List<UserDetails> userList = new ArrayList<>();
-//    userList.add(
-//            new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-//        userList.add(
-//                new User("ayse", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-//    return new InMemoryUserDetailsManager(userList);
-//    }// FIRSTLY  I CREATE AS A MANUALLY
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       // I use this SecurityFilterChain interface and HttpSecurity as a bean because I want to introduce my own validation from to Spring
+       // use SecurityFilterChain interface and HttpSecurity as a bean to introduce our own validation from to Spring
         return http
                 .authorizeRequests()
-               // .antMatchers("/user/**").hasRole("ADMIN") // ROLE_ADMIN--> if I want to hasRole I need to go to my database and I need to change the Role_..
-                .antMatchers("/user/**").hasAuthority("Admin") // in my app, certain roles should be able to see certain pages
-                .antMatchers("/project/**").hasAuthority("Manager")
-                .antMatchers("/task/employee/**").hasAuthority("Employee")
-                .antMatchers("/task/**").hasAuthority("Manager")
-               // .antMatchers("task/**").hasAnyRole("EMPLOYEE", "ADMIN") // I can DEFINE MORE THAN ONE ROLE , DEPENDS BUSINESS LOGIC, now only example we dont have more than roles
-               // .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")   // hasAuthority needs to be ROLE_,
-                .antMatchers( // antMatchers related with the pages
+                .antMatchers(
                         "/",
-                         "login",
+                         "/login",
                         "/fragments/**",
                         "/assets/**",
                         "/images/**"
-                ).permitAll() // everybody should be able to access to these pages, anybody can access
+                ).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                //.httpBasic()// I want to use my own login page so I command it
-                    .formLogin() // I want to introduce my own validation form to Spring
-                    .loginPage("/login")
-                    // .defaultSuccessUrl("/welcome")// whenever User is successfull done or User autoticated correct username and password this is the page we gonna land it,
-                // it is gonna navigate to welcome page
-                // I will modified which pages I will land it after log in the system, i will change default stuff
-                    .successHandler(authSuccessHandler)
-                    .failureUrl("/login?error=true") // if user put the wrong information username and password, I want to navigate to this URL
-                     .permitAll() // should be accessible by anyone
-                .and() // we put between this separate part .and()
+                //login functionality
+                    //will use formLogin() option
+                    .formLogin()
+                    .loginPage("/login")//End point will be "/login"
+                     // after login, user land to landing page based on the role (will be handled in "AuthSuccess Handler" class)
+                     .successHandler(authSuccessHandler)
+                //Logout functionality
+                    .failureUrl("/login?error=true") // for unsuccessful logins, the end point wil be "/login?error=true"
+                    .permitAll() // should be accessible by anyone
+                .and()
                     .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) //End point will be "/logout"
                     .logoutSuccessUrl("/login")
                 .and()
+                //"Remember Me" functionality
                     .rememberMe()
-                    .tokenValiditySeconds(120)
-                    .key("cydeo")
+                    .tokenValiditySeconds(864000) //Token will be valid for 864000 seconds (240 hours)
                     .userDetailsService(securityService)
                 .and().build(); // at the end finish everything we put .build()
     }
