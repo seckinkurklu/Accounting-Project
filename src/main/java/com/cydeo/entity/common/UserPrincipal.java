930a546858cad6f3d58fca63ad2b62be5c1842b3
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-24 16:24:29 +0300

    Implemented "UserDetails" class and override its methods

--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -12,6 +12,53 @@ import java.util.List;
 // to convert my user entity to spring security standard (UserDetails interface)
 
 public class UserPrincipal implements UserDetails {
+    // like as mapper,
+    // get to User from db or any user that you want to,
+    // and convert to UserDetails Spring understands only this (UserDetails)
+
+    private User user; // I am giving one User and convert to userDetails
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        GrantedAuthority authority = new SimpleGrantedAuthority(this.user.getRole().getDescription());
+        authorityList.add(authority);
+        return authorityList;
+    }
     
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+
 
 }
