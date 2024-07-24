4d21cae0c3bfcc28d3f730f58cd6f0209e88be9e
AbduShukur <abdushukur.sawut@gmail.com>
2024-07-24 11:48:40 +0300

    CategoryRepository,CategoryService & CategoryServiceImpl Update

---
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.cydeo.service;
+
+/**
+ * author:AbduShukur
+ * date:7/23/2024
+ */
+public interface CategoryService {
+}
---
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cydeo.service.impl;
+
+/**
+ * author:AbduShukur
+ * date:7/23/2024
+ */
+public class CategoryServiceImpl {
+}
