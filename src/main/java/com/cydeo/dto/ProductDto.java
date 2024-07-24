a803b59dd996cbceb06d03c06c9d2370cc8c1b32
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-24 09:53:47 +0300

    some updated

---
+++ src/main/java/com/cydeo/dto/ProductDto.java
@@ -0,0 +1,19 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.ProductUnit;
+import lombok.*;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+    private Long id;
+    private String name;
+    private Integer quantityInStock;
+    private Integer lowLimitAlert;
+    private ProductUnit productUnit;
+    private CategoryDto category;
+    private boolean hasProduct;
+}
