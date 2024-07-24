9dea25658e5b93ae5178ef950c3ac2be7531103f
AbduShukur <abdushukur.sawut@gmail.com>
2024-07-24 18:23:07 +0300

    CategoryService & CategoryServiceImpl update

--- src/main/java/com/cydeo/service/CategoryService.java
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -1,9 +1,16 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.dto.ClientVendorDto;
+
+import java.util.List;
+
 /**
  * author:AbduShukur
  * date:7/23/2024
  */
 public interface CategoryService {
+    List<CategoryDto> listAllCategory();
 
+    CategoryDto findById(Long id);
 }
--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,14 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.entity.Category;
+import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
+import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * author:AbduShukur
@@ -9,4 +16,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public List<CategoryDto> listAllCategory() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(category -> mapperUtil.convert(categories,CategoryDto.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDto findById(Long id) {
+        return categoryRepository.findById(id)
+                .map(category -> mapperUtil.convert(category, CategoryDto.class))
+                .orElse(null);
+    }
 }
