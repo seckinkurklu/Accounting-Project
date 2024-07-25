8dc8a32ca6ff0f2ec42b398923dd0e276bc02039
omrzcn <omrzcnn@gmail.com>
2024-07-25 12:18:55 +0300

    create method created.

--- src/main/java/com/cydeo/controller/ClientVendorController.java
+++ src/main/java/com/cydeo/controller/ClientVendorController.java
@@ -1,11 +1,16 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ClientVendorDto;
+import com.cydeo.enums.ClientVendorType;
 import com.cydeo.service.ClientVendorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/clientVendors")
@@ -26,6 +31,23 @@ public class ClientVendorController {
 
 
     }
+
+
+    @GetMapping("/create")
+    public String createClientVendor(Model model){
+
+        model.addAttribute("clientVendors",new ClientVendorDto());
+        List<ClientVendorType> clientVendorTypes = Arrays.asList(ClientVendorType.values());
+
+        model.addAttribute("clientVendorTypes", clientVendorTypes);
+
+
+
+
+        return "/clientVendor/clientVendor-create";
+
+    }
+
 
 
 
