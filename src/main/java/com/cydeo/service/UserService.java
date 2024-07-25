2865a9f90073a01b0f3b236503cacfebcf56c0b6
omrzcn <omrzcnn@gmail.com>
2024-07-25 22:53:39 +0300

    getLoggedUser method created

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.UserDto;
+import com.cydeo.entity.User;
 
 import java.util.List;
 
@@ -8,5 +9,8 @@ public interface UserService {
 
     UserDto findByUsername(String username);
     List<UserDto> listAllUsers();
+    UserDto getLoggedUser();
+
+
 
 }
