fcd005ece77469b7a2c1def182b94c82261ed6ac
omrzcn <omrzcnn@gmail.com>
2024-07-25 21:51:07 +0300

    update implemented

--- src/main/java/com/cydeo/service/Impl/ClientVendorServiceImpl.java
+++ src/main/java/com/cydeo/service/Impl/ClientVendorServiceImpl.java
@@ -6,6 +6,7 @@ import com.cydeo.service.ClientVendorService;
 import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -46,5 +47,26 @@ public class ClientVendorServiceImpl implements ClientVendorService {
         }
 
 //        clientVendorRepository.save(clientVendor);
+    }
+
+    @Override
+    public void update(ClientVendorDto clientVendorDto) {
+        if (clientVendorDto==null || clientVendorDto.getId()==null){
+            throw new IllegalArgumentException("ClientVendorDto or ClientVendor ID cannot be null");
+
+        }
+
+
+        ClientVendor clientVendor = clientVendorRepository.getReferenceById(clientVendorDto.getId());
+        if (clientVendor==null){
+            throw new EntityNotFoundException("Client/Vendor cannot be found with ID "+clientVendor.getId());
+
+        }
+
+        ClientVendor convertedClientVendor=mapperUtil.convert(clientVendorDto,ClientVendor.class);
+        convertedClientVendor.setId(clientVendor.getId());
+        convertedClientVendor.setAddress(clientVendor.getAddress());
+        clientVendorRepository.save(convertedClientVendor);
+
     }
 }
