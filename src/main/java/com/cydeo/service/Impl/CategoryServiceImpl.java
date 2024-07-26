3ed9f89fbd165db2d69db06bcfc5401041653bed
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-26 10:18:14 +0300

    CategoryServiceImpl is added

---
+++ src/main/java/com/cydeo/service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.entity.Category;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.CategoryService;
+import com.cydeo.util.MapperUtil;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * author:AbduShukur
+ * date:7/23/2024
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public List<CategoryDto> listAllCategory() {
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream().map(category -> mapperUtil.convert(category,new CategoryDto())).collect(Collectors.toList());
+    }
+
+}
