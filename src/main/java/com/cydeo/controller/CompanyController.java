1d3ef802c96aa3afa9c34bb6db6ba5be58ac556e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-26 23:27:17 +0300

    added /create endpoints and createCompany method

--- src/main/java/com/cydeo/controller/CompanyController.java
+++ src/main/java/com/cydeo/controller/CompanyController.java
@@ -1,10 +1,13 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.CompanyDto;
+import com.cydeo.entity.Company;
 import com.cydeo.service.CompanyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,5 +29,11 @@ public class CompanyController {
     public String createCompany(Model model) {
         model.addAttribute("company", new CompanyDto());
         return "company/company-create";
+    }
+    @PostMapping("/create")
+    public String createCompany(@ModelAttribute ("company") CompanyDto company, Model model) {
+        model.addAttribute("company", new CompanyDto());
+        companyService.save(company);
+        return "redirect:/companies/list";
     }
 }
