cee2476e7b2849c301ac1e9c83f1cb51fe83b474
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-26 09:28:40 +0300

    ClientVendorRepository is added

--- src/main/java/com/cydeo/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/repository/ClientVendorRepository.java
@@ -2,14 +2,12 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.ClientVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import javax.persistence.Id;
 import java.util.List;
 
-@Repository
-public interface ClientVendorRepository extends JpaRepository<ClientVendor,Long> {
+public interface ClientVendorRepository extends JpaRepository<ClientVendor, Long> {
 
-
+    List<ClientVendor> findAllByCompanyTitleOrderByClientVendorName(String companyTitle);
+    ClientVendor findByClientVendorName(String clientVendorName);
 
 }
