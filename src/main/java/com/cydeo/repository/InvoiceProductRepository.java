b71799c61bd6c766c667ecc9ded49af6b1f36c3f
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-26 09:28:52 +0300

    InvertProductRepository is added

--- src/main/java/com/cydeo/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/repository/InvoiceProductRepository.java
@@ -1,4 +1,15 @@
 package com.cydeo.repository;
 
-public interface InvoiceProductRepository {
+import com.cydeo.entity.Invoice;
+import com.cydeo.entity.InvoiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
+
+
+   List<InvoiceProduct> findAllByInvoice(Invoice invoice);
+
+   InvoiceProduct findByInvoice(Invoice invoice);
 }
