d1d29f5e6180ba34e9e2647ac6715a5ca1ebf10e
seckinkurklu <kurkluseckin@gmail.com>
2024-07-26 11:37:12 +0300

    Lazy annotation added

--- src/main/java/com/cydeo/converter/ProductDTOConverter.java
+++ src/main/java/com/cydeo/converter/ProductDTOConverter.java
@@ -3,6 +3,7 @@ package com.cydeo.converter;
 import com.cydeo.dto.ProductDto;
 import com.cydeo.service.ProductService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,7 @@ public class ProductDTOConverter implements Converter<String, ProductDto> {
     private final ProductService productService;
 
 
-    public ProductDTOConverter(ProductService productService) {
+    public ProductDTOConverter(@Lazy ProductService productService) {
         this.productService = productService;
     }
 
