c65e94d3f0b988900e613c0b2d408816e6f7b97f
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-26 09:29:14 +0300

    InvertPRepository is added

--- src/main/java/com/cydeo/repository/InvoiceRepository.java
+++ src/main/java/com/cydeo/repository/InvoiceRepository.java
@@ -1,14 +1,22 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Company;
 import com.cydeo.entity.Invoice;
+import com.cydeo.enums.InvoiceType;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
-public interface InvoiceRepository {
+public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
 
     List<Invoice> findAll();
-    Invoice findById(Long id);
+    //Invoice findById(Long id);
     Invoice save(Invoice invoice);
     void deleteById(Invoice invoice);
+
+    //for US-49
+    List<Invoice> findAllByInvoiceType_AndCompanyOrderByInvoiceNoDesc(InvoiceType invoiceType, Company company);
+
+
 }
