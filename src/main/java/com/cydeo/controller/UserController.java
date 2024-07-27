88ebbe6f91f2468bdb5de13de7a377fea0fb2621
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-27 03:52:14 +0300

    some fixed

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -11,19 +11,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
 @Controller
-@RequestMapping("/users")
+@RequestMapping ("/users")
 public class UserController {
 
     private final UserService userService;
-    private final RoleService roleService;
-    private final CompanyService companyService;
 
-    public UserController(UserService userService, RoleService roleService, CompanyService companyService) {
+
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
-        this.companyService = companyService;
     }
 
     @GetMapping("/list")
@@ -32,59 +28,4 @@ public class UserController {
         model.addAttribute("users", userService.getLoggedUser());
         return "/user/user-list";
     }
-
-    @GetMapping("/create")
-    public String CreateUser(Model model) {
-        model.addAttribute("newUser", new UserDto());
-        model.addAttribute("userRoles", roleService.listAllRoles());
-        model.addAttribute("companies", userService.listAllUsers());
-
-        return "/user/user-create";
-    }
-
-    @PostMapping("/create")
-    public String CreateUser(@Valid @ModelAttribute("newUser") UserDto user, BindingResult bindingResult, Model model) {
-        boolean emailExist = userService.findByUsernameCheck(user.getUsername());
-        if (bindingResult.hasErrors()) {
-            if (emailExist) {
-                bindingResult.rejectValue("username", " ", "A user with this email already exists. Please try with different email.");
-            }
-
-            model.addAttribute("userRoles", roleService.listAllRoles());
-            model.addAttribute("companies", userService.listAllUsers());
-
-            return "/user/user-create";
-        }
-        userService.save(user);
-        return "redirect:/users/list";
-    }
-
-    @GetMapping("/update/{id}")
-    public String UpdateUser(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-
-        model.addAttribute("userRoles", roleService.listAllRoles());
-        model.addAttribute("companies", companyService.listAllCompanies());
-        model.addAttribute("users", userService.listAllUsers());
-        return "/user/user-update";
-    }
-    @PostMapping("/update/{id}")
-    public String UpdateUser(@Valid @ModelAttribute("user") UserDto userDto,  BindingResult bindingResult, Model model,@PathVariable("id") Long id ) {
-       if (bindingResult.hasErrors()) {
-           model.addAttribute("userRoles", roleService.listAllRoles());
-           model.addAttribute("companies", companyService.listAllCompanies());
-           model.addAttribute("users", userService.listAllUsers());
-           return "/user/user-update";
-       }
-       userService.save(userDto);
-        return"redirect:/users/create";
-    }
-
 }
-
-
-
-
-
-
-
