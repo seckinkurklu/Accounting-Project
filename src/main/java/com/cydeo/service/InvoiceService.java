99ec48b15106cbc7dcab2d9c42141b2ec8422b93
omrzcn <omrzcnn@gmail.com>
2024-07-27 15:51:55 +0300

    fixed

--- src/main/java/com/cydeo/service/InvoiceService.java
+++ src/main/java/com/cydeo/service/InvoiceService.java
@@ -9,7 +9,7 @@ public interface InvoiceService {
     List<Invoice> getAllInvoices();
     Invoice getInvoiceById(Long id);
     Invoice createInvoice(Invoice invoice);
-    Invoice upodateInvoice(Long id,Invoice invoice);
+    Invoice updateInvoice(Long id,Invoice invoice);
     void deleteInvoice(Long id);
 
     // should use DTO class in Service
