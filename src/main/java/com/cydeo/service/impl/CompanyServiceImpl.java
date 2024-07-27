b50f85504aa1fcf8101aede2e1470a47b137813e
omrzcn <omrzcnn@gmail.com>
2024-07-27 15:51:17 +0300

    fixed

--- src/main/java/com/cydeo/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CompanyServiceImpl.java
@@ -8,6 +8,7 @@ import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CompanyServiceImpl implements CompanyService {
@@ -23,7 +24,7 @@ public class CompanyServiceImpl implements CompanyService {
     @Override
     public List<CompanyDto> listAllCompanies() {
          List<Company> companyList= companyRepository.findAll();
-        return companyList.stream().map(p->mapperUtil.convert(p, new CompanyDto())).toList();
+        return companyList.stream().map(p->mapperUtil.convert(p, new CompanyDto())).collect(Collectors.toList());
     }
 
     @Override
