3aa634e5542bf8e38b5b6b03e3ae47a56dccf8b3
omrzcn <omrzcnn@gmail.com>
2024-07-27 15:53:30 +0300

    create-update methods created

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -1,21 +1,26 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.ProductDto;
+import com.cydeo.enums.ProductUnit;
+import com.cydeo.service.CategoryService;
 import com.cydeo.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
 
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
+        this.categoryService = categoryService;
     }
 
 
@@ -26,4 +31,51 @@ public class ProductController {
 
         return "product/product-list";
     }
+
+
+    @GetMapping("/create")
+    public String createProduct(Model model){
+
+        model.addAttribute("newProduct",new ProductDto());
+        model.addAttribute("categories",categoryService.listAllCategory());
+        model.addAttribute("productUnits", ProductUnit.values());
+
+
+
+        return "product/product-create";
+
+    }
+
+    @PostMapping("/create")
+    public String insertProduct(@ModelAttribute("newProduct") ProductDto productDto, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.listAllCategory());
+            model.addAttribute("productUnits", ProductUnit.values());
+            return "product/product-create";
+        }
+        productService.save(productDto);
+
+        return "redirect:/products/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editProduct(@PathVariable("id") Long id,Model model){
+        model.addAttribute("product",productService.getProductById(id));
+        model.addAttribute("categories",categoryService.listAllCategory());
+        model.addAttribute("productUnits",ProductUnit.values());
+
+
+        return "product/product-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@ModelAttribute("product") ProductDto productDto){
+
+        productService.save(productDto);
+        return "redirect:/products/list";
+    }
+
+
+
 }
