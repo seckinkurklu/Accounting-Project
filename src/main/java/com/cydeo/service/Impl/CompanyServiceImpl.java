fc4bc8573ebf5079d3740357bd6fbd7be018d365
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-27 04:12:12 +0300

    added order by to findAll

--- src/main/java/com/cydeo/service/Impl/CompanyServiceImpl.java
+++ src/main/java/com/cydeo/service/Impl/CompanyServiceImpl.java
@@ -1,11 +1,13 @@
 package com.cydeo.service.Impl;
 
+import com.cydeo.dto.AddressDto;
 import com.cydeo.dto.CompanyDto;
 import com.cydeo.entity.Company;
 import com.cydeo.enums.CompanyStatus;
 import com.cydeo.repository.CompanyRepository;
 import com.cydeo.service.CompanyService;
 import com.cydeo.util.MapperUtil;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -23,7 +25,7 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public List<CompanyDto> listAllCompanies() {
-         List<Company> companyList= companyRepository.findAll();
+         List<Company> companyList= companyRepository.findAll(Sort.by(Sort.Direction.ASC, "title")); // Order by title Asc
         return companyList.stream().map(p->mapperUtil.convert(p, new CompanyDto())).toList();
     }
 
@@ -34,7 +36,20 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public void save(CompanyDto company) {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setAddressLine1(company.getAddress().getAddressLine1());
+        addressDto.setAddressLine2(company.getAddress().getAddressLine2());
+        addressDto.setCity(company.getAddress().getCity());
+        addressDto.setCountry(company.getAddress().getCountry());
+        addressDto.setZipCode(company.getAddress().getZipCode());
+        addressDto.setState(company.getAddress().getState());
+
+        company.setAddress(addressDto);
+        company.setTitle(company.getTitle());
+        company.setPhone(company.getPhone());
+        company.setWebsite(company.getWebsite());
         company.setCompanyStatus(CompanyStatus.PASSIVE);
-        companyRepository.save(mapperUtil.convert(company, new Company()));
+        Company converted = mapperUtil.convert(company, new Company());
+        companyRepository.save(converted);
     }
 }
