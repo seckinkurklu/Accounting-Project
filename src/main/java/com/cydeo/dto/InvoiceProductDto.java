db418417e26710c01441f1240513e874439f8fd5
Alim <laysuinfo@gmail.com>
2024-07-28 20:10:07 +0300

    InvoiceProductDto CLASS update

--- src/main/java/com/cydeo/dto/InvoiceProductDto.java
+++ src/main/java/com/cydeo/dto/InvoiceProductDto.java
@@ -13,7 +13,7 @@ public class InvoiceProductDto {
     private Long id;
     private Integer quantity;
     private BigDecimal price;
-    private Integer tax;
+    private BigDecimal tax;
     private BigDecimal total;
     private BigDecimal profitLoss;
     private Integer remainingQuantity;
