286b955e023af76620e469d2f89577e9270e4e0c
seckinkurklu <kurkluseckin@gmail.com>
2024-07-28 11:39:03 +0300

    Updated CompanyRepository

--- src/main/java/com/cydeo/repository/CompanyRepository.java
+++ src/main/java/com/cydeo/repository/CompanyRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
 }
