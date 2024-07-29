26c98d71dd7f37fb0184c830a5fee2d3905b4dcd
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-29 13:05:06 +0300

    listAllByCompany() is updated for US 41 to display hover over

--- src/main/java/com/cydeo/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/Impl/CategoryServiceImpl.java
@@ -7,7 +7,9 @@ import com.cydeo.entity.Category;
 import com.cydeo.entity.Company;
 import com.cydeo.entity.User;
 import com.cydeo.repository.CategoryRepository;
+import com.cydeo.repository.ProductRepository;
 import com.cydeo.service.CategoryService;
+import com.cydeo.service.CompanyService;
 import com.cydeo.service.UserService;
 import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
@@ -24,11 +26,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final MapperUtil mapperUtil;
     private final UserService userService;
+    private final ProductRepository productRepository;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil, UserService userService) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil, UserService userService, ProductRepository productRepository) {
         this.categoryRepository = categoryRepository;
         this.mapperUtil = mapperUtil;
         this.userService = userService;
+        this.productRepository = productRepository;
     }
 
     @Override
@@ -37,12 +41,24 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryList.stream().map(category -> mapperUtil.convert(category,new CategoryDto())).toList();
     }
 
-    @Override
+//    @Override
+//    public List<CategoryDto> listAllByCompany() {
+//        UserDto loggedUser = userService.getLoggedUser();
+//        CompanyDto companyDto = loggedUser.getCompany();
+//        List<Category> categoryList = categoryRepository.findAllByCompanyOrderByDescriptionAsc(mapperUtil.convert(companyDto, new Company()));
+//        return categoryList.stream().map(category -> mapperUtil.convert(category, new CategoryDto())).toList();
+//    }
+//---> listAllByCompany() is updated for US 41 to display hover over
     public List<CategoryDto> listAllByCompany() {
         UserDto loggedUser = userService.getLoggedUser();
-        CompanyDto companyDto = loggedUser.getCompany();
-        List<Category> categoryList = categoryRepository.findAllByCompanyOrderByDescriptionAsc(mapperUtil.convert(companyDto, new Company()));
-        return categoryList.stream().map(category -> mapperUtil.convert(category, new CategoryDto())).toList();
+        String title = loggedUser.getCompany().getTitle();
+        List<Category> categories = categoryRepository.findAllByCompany_Title(title);
+        return categories.stream().map(category -> {
+            CategoryDto categoryDto = mapperUtil.convert(category,new CategoryDto());
+            boolean hasProduct = productRepository.existsByCategory(category);
+            categoryDto.setHasProduct(hasProduct);
+            return categoryDto;
+        }).toList();
     }
 
     @Override
