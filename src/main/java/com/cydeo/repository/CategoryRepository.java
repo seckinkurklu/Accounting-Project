913b1f1141ca331527353e393befefd7d8056801
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-29 13:07:32 +0300

    added findAllByCompany_Title in CategoryRepository

--- src/main/java/com/cydeo/repository/CategoryRepository.java
+++ src/main/java/com/cydeo/repository/CategoryRepository.java
@@ -15,4 +15,6 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findAllByCompanyOrderByDescriptionAsc(Company company);
+
+    List<Category> findAllByCompany_Title(String title);
 }
