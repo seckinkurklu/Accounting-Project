d1e6efba203f68e5fefa8365118caf60e7f4ffd9
AbduShukur <abdushukur.sawut@gmail.com>
2024-07-30 18:33:44 +0300

    AddressDto and CompanyDto update

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -1,6 +1,9 @@
 package com.cydeo.dto;
 
 import lombok.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @ToString
 @NoArgsConstructor
@@ -9,10 +12,27 @@ import lombok.*;
 @Setter
 public class AddressDto {
     private Long id;
+
+    @NotBlank(message = "Address is required field.")
+    @Size(min = 2, max = 100, message = "Address should have 2-100 characters long.")
     private String addressLine1;
+
+    @Size(max = 100, message = "Address should have maximum 100 characters long.")
     private String addressLine2;
+
+    @NotBlank(message = "City is a required field.")
+    @Size(min = 2, max = 50, message = "City should have 2-50 characters long.")
     private String city;
+
+    @NotBlank(message = "State is a required field.")
+    @Size(min = 2, max = 50, message = "State should have 2-50 characters long.")
     private String state;
+
+    @NotBlank(message = "Country is a required field.")
+    @Size(min = 2, max = 50, message = "Country should have 2-50 characters long.")
     private String country;
+
+    @NotBlank(message = "Zipcode is a required field.")
+    @Pattern(regexp = "\\d{5}-\\d{4}", message = "Zipcode should have a valid form.")
     private String zipCode;
 }
