21c6ff942f6ea6a406b678299cbce940fef52447
omrzcn <omrzcnn@gmail.com>
2024-07-30 17:18:42 +0300

    DashboardServiceImpl is created

---
+++ src/main/java/com/cydeo/service/s_impl/DashboardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cydeo.service.s_impl;
+
+import com.cydeo.service.DashboardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardServiceImpl implements DashboardService {
+}
