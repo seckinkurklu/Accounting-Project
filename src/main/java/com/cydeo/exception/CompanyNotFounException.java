5d2c0bf37c6ef51360141871d2142ba0e0cff54e
omrzcn <omrzcnn@gmail.com>
2024-08-01 10:38:16 +0300

    name is corrected

--- src/main/java/com/cydeo/exception/CompanyNotFounException.java
+++
@@ -1,7 +0,0 @@
-package com.cydeo.exception;
-
-public class CompanyNotFounException extends  RuntimeException {
-   public CompanyNotFounException(String message){
-        super(message);
-    }
-}
