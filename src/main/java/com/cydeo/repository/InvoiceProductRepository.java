9d86f553c51f82ccffcdfce68d171729118fe6f2
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-02 01:55:24 +0300

    NativeQuery is created for month and Profit/Loss

--- src/main/java/com/cydeo/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/repository/InvoiceProductRepository.java
@@ -2,10 +2,14 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Invoice;
 import com.cydeo.entity.InvoiceProduct;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
+
 @Repository
 public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
 
@@ -20,6 +24,10 @@ public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, 
    InvoiceProduct findByIdAndIsDeleted(Long id, Boolean isDeleted);
 
    List<InvoiceProduct> findAllByIsDeletedFalse();
+
+   @Query(value = "select i.date, ip.profit_loss from invoices i join invoice_products ip on i.id = ip.invoice_id\n" +
+           "join companies c on i.company_id = c.id where c.title=?1", nativeQuery = true)
+   List<Object[]> dateAndProfitLossByCompanyTitle(@Param("title") String companyTitle);
 
 
 }
