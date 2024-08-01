c47bc395b569c163304173df12411b5c2695d575
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-02 01:29:39 +0300

    ReportService is created

--- src/main/java/com/cydeo/controller/ReportController.java
+++ src/main/java/com/cydeo/controller/ReportController.java
@@ -1,9 +1,18 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/reports")
 public class ReportController {
+
+    @GetMapping("/profitLossData")
+    public String profitLossData(Model model) {
+        model.addAttribute("monthlyProfitLossDataMap", )
+        return "report/profitLossData";
+    }
 }
---
+++ src/main/java/com/cydeo/service/s_impl/ReportServiceImpl.java
@@ -0,0 +1,4 @@
+package com.cydeo.service.s_impl;
+
+public class ReportServiceImpl {
+}
