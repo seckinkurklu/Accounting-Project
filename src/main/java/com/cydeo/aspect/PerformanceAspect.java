ed93325132efdcb85fca1fbb64daf081b1610ba8
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-02 23:14:02 +0300

    Refactor ExecutionTimeAspect to PerformanceAspect

--- src/main/java/com/cydeo/aspect/PerformanceAspect.java
+++ src/main/java/com/cydeo/aspect/PerformanceAspect.java
@@ -1,19 +1,15 @@
 package com.cydeo.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
-public class ExecutionTimeAspect {
+public class PerformanceAspect {
 
    //private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeAspect.class);
 
