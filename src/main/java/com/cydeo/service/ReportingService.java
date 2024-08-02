55444af7fd88560c77880e69337bbaa55c0cfe4b
Alim <laysuinfo@gmail.com>
2024-08-03 01:13:10 +0300

    ReportingService added some method

--- src/main/java/com/cydeo/service/ReportingService.java
+++ src/main/java/com/cydeo/service/ReportingService.java
@@ -1,9 +1,14 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.InvoiceProductDto;
+
+import java.util.List;
 import java.util.Map;
 
 public interface ReportingService {
 
 
     Map<String,Double> getMonthlyProfitLossByCompany();
+
+   List<InvoiceProductDto> getInvoiceProductList();
 }
