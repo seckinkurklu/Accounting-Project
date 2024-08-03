fbb23bbdfe4c5ada70aebb77bc4403ba9621899f
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-03 13:35:20 +0300

    Modified JDGR10-63_Dashboard_see-current-exchange

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -1,27 +1,25 @@
 package com.cydeo.controller;
 
-import com.cydeo.client.CurrencyClient;
-import com.cydeo.service.CurrencyService;
+import com.cydeo.service.DashboardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-    private final CurrencyService currencyService;
+    private final DashboardService dashboardService;
 
-    public DashboardController(CurrencyService currencyService) {
-        this.currencyService = currencyService;
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
     }
 
     @GetMapping()
     public String getExchangeRates(Model model) {
 
-        model.addAttribute("exchangeRates", currencyService.getExchangeRates());
+        model.addAttribute("exchangeRates",dashboardService.getExchangeRates());
 
         return "/dashboard";
 
