eb446b08e00c4277e1012431855f4ca9023c7316
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-05 19:19:13 +0300

    created payments/list endpoints

--- src/main/java/com/cydeo/controller/PaymentController.java
+++ src/main/java/com/cydeo/controller/PaymentController.java
@@ -2,6 +2,8 @@ package com.cydeo.controller;
 
 import com.cydeo.service.PaymentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,11 @@ public class PaymentController {
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
+    }
+
+    @GetMapping("/list")
+    public String paymentsList(Model model) {
+        model.addAttribute("payments", paymentService.findAll());
+        return "/payment/payment-list";
     }
 }
