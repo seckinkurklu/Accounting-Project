7664f65065964c1ffdf31b86b2ab6da7169674f3
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-06 18:05:57 +0300

    created findAllByYear method

--- src/main/java/com/cydeo/repository/PaymentRepository.java
+++ src/main/java/com/cydeo/repository/PaymentRepository.java
@@ -3,8 +3,10 @@ package com.cydeo.repository;
 import com.cydeo.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-
+    List<Payment> findAllByYear(int year);
 
 }
