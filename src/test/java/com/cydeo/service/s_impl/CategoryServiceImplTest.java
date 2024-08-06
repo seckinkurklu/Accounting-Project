ce27a8f24ce8fdc20840379255b7f8a0523d11a3
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-06 22:05:06 +0300

    created CategoryServiceImplTest

---
+++ src/test/java/com/cydeo/service/s_impl/CategoryServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.cydeo.service.s_impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceImplTest {
+
+    @Test
+    void listAllCategory() {
+    }
+
+    @Test
+    void listAllByCompany() {
+    }
+
+    @Test
+    void findCategoryById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findByDescription() {
+    }
+
+    @Test
+    void existCategory() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getCategoryById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void getCategoriesForCurrentUser() {
+    }
+}
