a212ae3acad45084046056d69238b08225f2a669
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-07 15:46:46 +0300

    created Mock object

--- src/test/java/com/cydeo/service/s_impl/CategoryServiceImplTest.java
+++ src/test/java/com/cydeo/service/s_impl/CategoryServiceImplTest.java
@@ -1,13 +1,39 @@
 package com.cydeo.service.s_impl;
 
+import com.cydeo.dto.UserDto;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.UserService;
+import com.cydeo.util.MapperUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+    @Mock
+    MapperUtil mapperUtil;
+
+    @Mock
+    UserService userService;
+
+    @InjectMocks
+    CategoryServiceImpl categoryService;
 
     @Test
     void listAllCategory() {
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        categoryService.listAllCategory();
+        verify(categoryRepository).findDistinctCategoriesByCompanyAndIsDeletedFalse(1L);
+
     }
 
     @Test
@@ -46,3 +72,4 @@ class CategoryServiceImplTest {
     void getCategoriesForCurrentUser() {
     }
 }
+
