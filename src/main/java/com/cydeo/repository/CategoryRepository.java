bfa676bdaca8a0b61c24d8790afd2677f3c3b604
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-07 15:38:03 +0300

    findByDescriptionAndCompany_Title method created

--- src/main/java/com/cydeo/repository/CategoryRepository.java
+++ src/main/java/com/cydeo/repository/CategoryRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * author:AbduShukur
@@ -20,7 +21,8 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByCompanyIdOrderByDescriptionAsc(@Param("companyId") Long companyId);
 
     List<Category> findAllByCompany_Title(String title);
-    Category findByDescription(String description);
+    Optional<Category> findByDescriptionAndCompany_Title(String description, String title);
+
 
 
     @Query("SELECT DISTINCT c FROM Category c WHERE c.company.id = :companyId AND c.isDeleted = false ORDER BY c.description ASC")
