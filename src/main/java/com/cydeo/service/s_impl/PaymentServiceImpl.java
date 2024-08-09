9661e34534c23bfb6e4ca802abcbf69b9dd7c198
omrzcn <omrzcnn@gmail.com>
2024-08-09 15:09:58 +0300

    processPayment method is implemented

--- src/main/java/com/cydeo/service/s_impl/PaymentServiceImpl.java
+++ src/main/java/com/cydeo/service/s_impl/PaymentServiceImpl.java
@@ -1,15 +1,20 @@
 package com.cydeo.service.s_impl;
 
+import com.cydeo.client.StripeClient;
 import com.cydeo.dto.PaymentDto;
+import com.cydeo.dto.stripe_api.PaymentRequest;
+import com.cydeo.dto.stripe_api.PaymentResponse;
 import com.cydeo.entity.Payment;
+import com.cydeo.enums.Currency;
 import com.cydeo.enums.Months;
+import com.cydeo.exception.PaymentNotFoundException;
 import com.cydeo.repository.PaymentRepository;
 import com.cydeo.service.PaymentService;
 import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -20,10 +25,13 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final MapperUtil mapperUtil;
+    private final StripeClient stripeClient;
+    private final String stripeSecretKey="sk_test_51Pj1miJfAsxFag9wDrFBgM900trAZmEjbBoboPw78KNl6AysGXwNO4PlgqDWLVU4gkEbIdb862DXW9650lQKfQdD00AZZGcWOF";
 
-    public PaymentServiceImpl(PaymentRepository paymentRepository, MapperUtil mapperUtil) {
+    public PaymentServiceImpl(PaymentRepository paymentRepository, MapperUtil mapperUtil, StripeClient stripeClient) {
         this.paymentRepository = paymentRepository;
         this.mapperUtil = mapperUtil;
+        this.stripeClient = stripeClient;
     }
 
     @Override
@@ -70,6 +78,32 @@ public class PaymentServiceImpl implements PaymentService {
         return payments.stream().map(p->mapperUtil.convert(p, new PaymentDto()))
                 .sorted(Comparator.comparing(payment -> payment.getMonth().ordinal())).toList();
     }
+
+
+
+    @Override
+    public PaymentDto processPayment(PaymentDto paymentDto) {
+        int amount = paymentDto.getAmount().multiply(new BigDecimal(100)).intValue();
+        String currency = "usd";
+        String source = paymentDto.getCompanyStripeId();
+        String description = paymentDto.getDescription();
+
+        String authorizationHeader = "Bearer " + stripeSecretKey;
+        PaymentResponse response = stripeClient.createCharge(amount, currency, source, description, authorizationHeader);
+
+        if (response.isPaid()) {
+            paymentDto.setPaid(true);
+            paymentDto.setCompanyStripeId(response.getId());
+            paymentDto.setDescription( response.getDescription());
+        } else {
+            paymentDto.setPaid(false);
+            paymentDto.setDescription("Transaction failed. Please try again!");
+        }
+
+        return paymentDto;
+    }
+
+
 
     private void createPaymentsForYear(int year) {
         List<Payment> payments = new ArrayList<>();
