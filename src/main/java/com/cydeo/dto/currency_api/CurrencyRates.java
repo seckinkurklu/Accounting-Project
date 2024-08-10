c193a4ff29107539de2bd28f321ab9b69bf0769b
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-08-10 14:37:20 +0300

    fixed bug about in UI unique message

--- src/main/java/com/cydeo/dto/currency_api/CurrencyRates.java
+++ src/main/java/com/cydeo/dto/currency_api/CurrencyRates.java
@@ -5,10 +5,12 @@ package com.cydeo.dto.currency_api;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrencyRates {
 
